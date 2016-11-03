85239e1ebe06f05a1096a00ad3d2d1b9a33e7b10
rclations <contact@rclations.com>
2016-11-03 17:48:42 -0400

    oop bugfixes

--- client-hosting-manager.php
+++ client-hosting-manager.php
@@ -30,12 +30,15 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-$org = array(
-	'name' => 'INN',
-	'support_email' => 'support@largoproject.org',
-	'admin_email' => 'largo@investigativenewsnetwork.org',
-	'domains' => array( 'inn.org', 'investigativenewsnetwork.org' )
-);
+function client_hosting_manager_get_org_data() {
+	$org = array(
+		'name' => 'INN',
+		'support_email' => 'support@largoproject.org',
+		'admin_email' => 'largo@investigativenewsnetwork.org',
+		'domains' => array( 'inn.org', 'investigativenewsnetwork.org' )
+	);
+	return $org;
+}
 
 /**
  * The code that runs during plugin activation.
@@ -44,6 +47,7 @@ $org = array(
  * Check for any admins that have org emails and grant their accounts the capabilities
  */
 function activate_client_hosting_manager() {
+	$org = client_hosting_manager_get_org_data();
 
 	$administrator = get_role( 'administrator' );
 	client_hosting_manager_update_caps( 'remove', $administrator );
@@ -52,11 +56,11 @@ function activate_client_hosting_manager() {
 	foreach ( $admins as $admin ) {
 		foreach ( $org['domains'] as $domain ) {
 			if ( strpos( $admin->data->user_email, $domain ) != false ) {
-				$user = new WP_User( $admin->data_user_email );
+				$user = get_user_by( 'email', $admin->data->user_email );
 				client_hosting_manager_update_caps( 'add', $user );
 			}
 		}
-	}	
+	}
 }
 register_activation_hook( __FILE__, 'activate_client_hosting_manager' );
 
@@ -66,7 +70,8 @@ register_activation_hook( __FILE__, 'activate_client_hosting_manager' );
  * Restore default capabilities for the admin role
  * Check for any admins that have org emails and remove overridden capabilities
  */
-function deactivate_client_hosting_manager() {
+function deactivate_client_hosting_manager( $org ) {
+	$org = client_hosting_manager_get_org_data();
 
 	$administrator = get_role( 'administrator' );
 	client_hosting_manager_update_caps( 'add', $administrator );
@@ -75,11 +80,11 @@ function deactivate_client_hosting_manager() {
 	foreach ( $admins as $admin ) {
 		foreach ( $org['domains'] as $domain ) {
 			if ( strpos( $admin->data->user_email, $domain ) != false ) {
-				$user = new WP_User( $admin->data_user_email );
+				$user = get_user_by( 'email', $admin->data->user_email );
 				client_hosting_manager_update_caps( 'remove', $user );
 			}
 		}
-	}	
+	}
 }
 register_deactivation_hook( __FILE__, 'deactivate_client_hosting_manager' );
 
@@ -96,10 +101,10 @@ function client_hosting_manager_update_caps( $action, $object ) {
 	foreach ( $caps as $cap ) {
 		switch ( $action ) {
 			case 'add':
-				$administrator->add_cap( $cap );
+				$object->add_cap( $cap );
 				break;
 			case 'remove':
-				$administrator->remove_cap( $cap );
+				$object->remove_cap( $cap );
 				break;
 		}
 	}
@@ -107,27 +112,11 @@ function client_hosting_manager_update_caps( $action, $object ) {
 
 class Client_Hosting_Manager {
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function __construct() {
-
-		$this->plugin_name = 'client-hosting-manager';
-		$this->version = '1.0.0';
-
-		$this->load_dependencies();
-		$this->set_locale();
-		$this->define_admin_hooks();
-		$this->define_public_hooks();
-
+		add_action( 'delete_user', array( $this, 'delete_user' ), 10, 1 );
+		add_action( 'user_register', array( $this, 'user_registration' ), 10, 1 );
+		add_action( 'set_user_role', array( $this, 'user_role_update' ), 10, 3 );
 	}
-
 
 	/**
 	 * Prevent org's master user from being deleted.
@@ -137,7 +126,7 @@ class Client_Hosting_Manager {
 	 */
 	public function delete_user( $user_id ) {
 		global $wpdb, $org;
-		$user_obj = get_userdata( $user_id );	
+		$user_obj = get_userdata( $user_id );
 		if ( $user_obj->user_email ===  $org['admin_email'] ) {
 			wp_die( 'You do not have permissions to delete ' . $org['name'] . ' users. Please contact <a href="mailto:' . $org['support_email'] . '">' . $org['support_email'] . '</a> for more information.' );
 		}
@@ -150,8 +139,7 @@ class Client_Hosting_Manager {
 	 * @param      int    $user_id    	The new user's id.
 	 */
 	public function user_registration( $user_id ) {
-		global $org;
-
+		$org = client_hosting_manager_get_org_data();
 		$user = get_userdata( $user_id );
 		foreach ( $org['domains'] as $domain ) {
 			if ( strpos( $user->user_email, $domain ) != false && in_array( 'administrator', $user->roles ) ) {
@@ -166,11 +154,11 @@ class Client_Hosting_Manager {
 	 *
 	 * @since      1.0.0
 	 * @param      int	$user_id       The name of this plugin.
-	 * @param      string   $old_role      The user's old role. 
+	 * @param      string   $old_role      The user's old role.
 	 * @param      string   $new_role      The user's new role.
 	 */
 	public function user_role_update( $user_id, $old_role, $new_role ) {
-		global $org;
+		$org = client_hosting_manager_get_org_data();
 
 		if ( 'administrator' == $new_role && 'administrator' != $old_role ) {
 			$user = get_userdata( $user_id );
@@ -187,11 +175,8 @@ class Client_Hosting_Manager {
 					client_hosting_manager_update_caps( 'remove', $user );
 				}
 			}
-		}	
+		}
 	}
 }
 
 $plugin = new Client_Hosting_Manager();
-add_action( 'delete_user', $plugin->delete_user, 10, 1 );
-add_action( 'user_register', $plugin->user_registration, 10, 1 );
-add_action( 'set_user_role', $plugin->user_role_update, 10, 3 );
